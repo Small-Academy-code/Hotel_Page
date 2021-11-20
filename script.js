4263c007bd66bab0fb791653998f34d0692ef95b
Small-Academy-code <73087944+Small-Academy-code@users.noreply.github.com>
2021-11-20 19:47:16 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+const fc1 = document.getElementById("fc1");
+fc1.onmouseover = () => {
+  document.getElementById("p1").style.display = "block";
+};
+const fc2 = document.getElementById("fc2");
+fc2.onmouseover = () => {
+  document.getElementById("p2").style.display = "block";
+};
+const fc3 = document.getElementById("fc3");
+fc3.onmouseover = () => {
+  document.getElementById("p3").style.display = "block";
+};
+const fc4 = document.getElementById("fc4");
+fc4.onmouseover = () => {
+  document.getElementById("p4").style.display = "block";
+};
